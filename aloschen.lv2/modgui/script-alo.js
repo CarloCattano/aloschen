31bd6201dbce3788b30e7ef3f2dcd00a1baf6425
CarloCattano <17380530+CarloCattano@users.noreply.github.com>
2026-03-11 23:06:11 +0100

    round UI to int and fish threshold

--- aloschen.lv2/modgui/script-alo.js
+++ aloschen.lv2/modgui/script-alo.js
@@ -269,12 +269,19 @@ function (event, funcs) {
         } else if (symbol === 'split_by_transient') {
             /* remember split mode so other handlers can act on it */
             data.split_by_transient = value;
+        } else if (symbol === 'transient_threshold') {
+            data.transient_threshold = value;
+
+            var thresholdNodes = icon.find('[mod-role="input-control-value"][mod-port-symbol="transient_threshold"]');
+            if (thresholdNodes && thresholdNodes.length) {
+                thresholdNodes.text(String(Math.round(value)));
+            }
         } else if (symbol === 'slice_env_attack' || symbol === 'slice_env_frac') {
             data[symbol] = value;
 
             var inputNodes = icon.find('[mod-role="input-control-value"][mod-port-symbol="' + symbol + '"]');
             if (inputNodes && inputNodes.length) {
-                inputNodes.text(String(value));
+                inputNodes.text(String(Math.round(value)));
             }
         } else if (symbol === 'detected_slices') {
             data.detected_slices = value;
@@ -321,11 +328,11 @@ function (event, funcs) {
                 }
             }
 
-            ['slice_env_attack', 'slice_env_frac'].forEach(function(symbol) {
+            ['transient_threshold', 'slice_env_attack', 'slice_env_frac'].forEach(function(symbol) {
                 if (typeof data[symbol] === 'number') {
                     var inputNodesInit = icon.find('[mod-role="input-control-value"][mod-port-symbol="' + symbol + '"]');
                     if (inputNodesInit && inputNodesInit.length) {
-                        inputNodesInit.text(String(data[symbol]));
+                        inputNodesInit.text(String(Math.round(data[symbol])));
                     }
                 }
             });
